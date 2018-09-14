1523653f609625b9855b2afc41230256bbcbf6ef
Utsav Shukla <us241077@gmail.com>
2018-09-15 01:00:17 +0530

    first commit

---
+++ ocr.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import os
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import pandas as pd
+import unicodecsv as csv
+ 
+
+# ASCII
+asciicodes = [' ','!','"','#','$','%','&','','(',')','*','+',',','-','.','/',
+          '0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@',
+          'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+          'r','s','t','u','v','w','x','y','z','[','\\',']','^','_','\n']
+print type(asciicodes)
+# Braille symbols
+brailles = ['⠀','⠮','⠐','⠼','⠫','⠩','⠯','⠄','⠷','⠾','⠡','⠬','⠠','⠤','⠨','⠌','⠴','⠂','⠆','⠒','⠲','⠢',
+        '⠖','⠶','⠦','⠔','⠱','⠰','⠣','⠿','⠜','⠹','⠈','⠁','⠃','⠉','⠙','⠑','⠋','⠛','⠓','⠊','⠚','⠅',
+        '⠇','⠍','⠝','⠕','⠏','⠟','⠗','⠎','⠞','⠥','⠧','⠺','⠭','⠽','⠵','⠪','⠳','⠻','⠘','⠸','\n']
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+	help="type of preprocessing to be done")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+if args["preprocess"] == "thresh":
+	gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+elif args["preprocess"] == "blur":
+	gray = cv2.medianBlur(gray, 3)
+
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
+
+print("=================\n=================")
+p=[]
+for alphabet in text:
+	k=brailles[asciicodes.index(alphabet.lower())].format('utf-8')
+	k=str(k)
+	p.append(k)
+
+df = pd.DataFrame({'col':p})
+df.to_csv('results.csv', index=False, header=False)
+print df
+
+cv2.imshow("Image", image)
+cv2.imshow("Output", gray)
+cv2.waitKey(0)
